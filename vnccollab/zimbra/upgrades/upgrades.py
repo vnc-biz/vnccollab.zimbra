347478e1da1f864db6d12a6c4e00334f621207f4
José Dinuncio <jose.dinuncio@gmail.com>
2013-08-16 10:44:47 -0430

    Replaces theme's portlets.

--- vnccollab/zimbra/upgrades/upgrades.py
+++ vnccollab/zimbra/upgrades/upgrades.py
@@ -3,6 +3,7 @@ from plone.app.upgrade.utils import installOrReinstallProduct
 from plone import api
 
 from vnccollab.zimbra.config import PROJECTNAME
+from vnccollab.zimbra.upgrades.replaceportlets import replace_all_portlets
 
 
 DEFAULT_PROFILE = 'profile-%s:default' % PROJECTNAME
@@ -12,3 +13,8 @@ def upgrade_1001_1002(context):
     '''Installs vnccollab.common.'''
     portal = api.portal.get()
     installOrReinstallProduct(portal, 'vnccollab.common')
+
+
+def upgrade_1001_1002_replace_portlets(context):
+    '''Replace Theme portlets.'''
+    replace_all_portlets()
