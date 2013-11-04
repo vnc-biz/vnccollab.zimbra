d96d1e2cd82ceddcd24d59cd7e9ea52590195b0f
José Dinuncio <jose.dinuncio@gmail.com>
2013-11-04 18:38:17 -0430

    Adds dependencies and format to setup.py.

--- setup.py
+++ setup.py
@@ -3,37 +3,39 @@ import os
 
 version = '1.0.2'
 
-setup(name='vnccollab.zimbra',
-      version=version,
-      description="VNC Collaboration Zimbra AddOn.",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      classifiers=[
+setup(
+    name='vnccollab.zimbra',
+    version=version,
+    description="VNC Collaboration Zimbra AddOn.",
+    long_description=open("README.txt").read() + "\n" +
+                     open(os.path.join("docs", "HISTORY.txt")).read(),
+    classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
-        ],
-      keywords='',
-      author='Jose Dinuncio',
-      author_email='jose.dinuncio@vnc.biz',
-      url='http://svn.plone.org/svn/collective/',
-      license='gpl',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['vnccollab'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'pyzimbra',
-          'five.grok',
-          'plone.api',
-          'BeautifulSoup',
-          'collective.z3cform.datepicker',
-          'vnccollab.common',
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
+    ],
+    keywords='',
+    author='Jose Dinuncio',
+    author_email='jose.dinuncio@vnc.biz',
+    url='http://svn.plone.org/svn/collective/',
+    license='gpl',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['vnccollab'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'pyzimbra',
+        'five.grok',
+        'plone.api',
+        'BeautifulSoup',
+        'collective.customizablePersonalizeForm',
+        'collective.z3cform.datepicker',
+        'vnccollab.common',
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
 
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      )
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
